08c173e7688c666e5a586087a53659e559e11142
Lucas M <lucas.msantos68@senacsp.edu.br>
2022-04-05 12:59:06 -0300

    feat(controllers): adiciona TesteController

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import TestController from './App/controllers/TestController';
+
 const routes = new Router();
+
+routes.get('/users', TestController.index);
 
 export default routes;
