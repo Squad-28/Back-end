0cf69fd86e5fe93feba846d10eab05981d7dcd5b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-07 13:46:55 -0300

    formatando a classe TestController e UsersController

---
+++ src/controllers/Test.controller.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+
+class TestController {
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      if (users < 1) {
+        return res
+          .status(200)
+          .json({ message: 'There are no registered users.' });
+      }
+
+      return res.json(users);
+    } catch (err) {
+      console.error(err);
+
+      return res.status(500).json({ error: 'Internal server error.' });
+    }
+  }
+}
+
+export default new TestController();
