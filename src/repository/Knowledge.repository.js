396f47ce66656fae413c40d0c8d23a8d8137ce64
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-09 19:36:08 -0300

    repository.knowledge: cria funcao bulkCreate

--- src/repository/Knowledge.repository.js
+++ src/repository/Knowledge.repository.js
@@ -1,3 +1,23 @@
-class KnowledgeRepository {}
+import database from '../database';
+import Knowledge from '../models/Knowledge';
+
+const formatReturn = (result) => {
+  const data = result.dataValues;
+  delete data?.createdAt;
+  delete data?.updatedAt;
+  return data;
+};
+
+class KnowledgeRepository {
+  async bulkCreate(knowledges, transaction = null) {
+    try {
+      await Knowledge.bulkCreate(knowledges, {
+        transaction,
+      });
+    } catch (error) {
+      console.log('[ERRO NO BD, BULK CREATE]: ' + error);
+    }
+  }
+}
 
 export default new KnowledgeRepository();
