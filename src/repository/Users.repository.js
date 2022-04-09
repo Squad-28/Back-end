4f67e07cb0a90eb31b9912ca8a47666749dd8c5f
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-09 19:34:24 -0300

    repository.users: cria funcao create

--- src/repository/Users.repository.js
+++ src/repository/Users.repository.js
@@ -1,12 +1,27 @@
 import User from '../models/User';
 
-const formatarRetorno = (result) => {
+const formatReturn = (result) => {
   const data = result.dataValues;
   delete data?.createdAt;
   delete data?.updatedAt;
   return data;
 };
 
-class UsersRepository {}
+class UsersRepository {
+  async create(user, transaction = null) {
+    try {
+      const instance = new User(user);
+      let userSaved = await instance.save({ transaction });
+
+      userSaved = formatReturn(userSaved);
+
+      // console.log('user: ', userSaved);
+
+      return userSaved;
+    } catch (error) {
+      console.log('[ERRO NO BD, CREATE]: ' + error);
+    }
+  }
+}
 
 export default new UsersRepository();
