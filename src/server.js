8faebc8e95e3df733fe44c24566111b101fae0b1
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-11 16:00:57 -0300

    src.server: usando metodo pra buscar o server e iniciar o servidor com express

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
-import app from './app';
+import App from './app';
+import Database from './database';
 
-app.listen(3333, () =>
-  console.log('🚀 Server started at http://localhost:3333')
-);
+const app = new App(new Database());
+
+app
+  .getServer()
+  .listen(3333, () =>
+    console.log('🚀 Server started at http://localhost:3333')
+  );
