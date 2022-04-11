2e173d67c9090cd12ff3194887ba5c7bfb9d5fb9
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-11 16:42:55 -0300

    src.routes: instanciando controller na rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,15 @@ import TestController from '../controllers/Test.controller';
 import UsersController from '../controllers/Users.controller';
 
 const routes = new Router();
+const usersController = new UsersController();
 
 // test route
 routes.get('/test', TestController.findAll);
 
 // app routes
-routes.get('/users', UsersController.index);
-routes.get('/users/:id', UsersController.show);
-routes.post('/users', UsersController.create);
-routes.put('/users/:id', UsersController.update);
+// routes.get('/users', usersController.index);
+// routes.get('/users/:id', usersController.show);
+routes.post('/users', usersController.create);
+// routes.put('/users/:id', usersController.update);
 
 export default routes;
