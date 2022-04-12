a9c43d8327bd2b1fe8239312f38963c580cfeefa
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-12 19:25:37 -0300

    src.routes: remove o TesteController e adiciona o index do UsersController

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,12 @@
 import { Router } from 'express';
 
-import TestController from '../controllers/Test.controller';
 import UsersController from '../controllers/Users.controller';
 
 const routes = new Router();
 const usersController = new UsersController();
 
-// test route
-routes.get('/test', TestController.findAll);
-
 // app routes
-// routes.get('/users', usersController.index);
+routes.get('/users', usersController.index);
 // routes.get('/users/:id', usersController.show);
 routes.post('/users', usersController.create);
 // routes.put('/users/:id', usersController.update);
