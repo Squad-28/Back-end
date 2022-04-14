d93d50fedd9b70a7e1461d97e06150f63c6d2cc2
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-14 02:07:05 -0300

    services.users.IndexUserService: remove o metodo formatAllUsers

--- src/services/users/IndexUser.service.js
+++ src/services/users/IndexUser.service.js
@@ -1,95 +1,25 @@
+import formatAllUsers from '../../utils/formatAllUsers';
+
 class IndexUserService {
   #sequelize;
   #usersRepo;
-  #knowledgeRepo;
-  #knowledgeListRepo;
 
-  constructor(sequelize, usersRepo, knowledgeRepo, knowledgeListRepo) {
+  constructor(sequelize, usersRepo) {
     this.#sequelize = sequelize;
     this.#usersRepo = usersRepo;
-    this.#knowledgeRepo = knowledgeRepo;
-    this.#knowledgeListRepo = knowledgeListRepo;
   }
 
   async index() {
     try {
       const result = await this.#usersRepo.index();
 
-      const users = this.#formatAllUsers(result);
+      const users = formatAllUsers(result);
 
       return users;
     } catch (error) {
       console.log(error);
       throw error;
     }
-  }
-
-  #formatAllUsers(usersSearched = []) {
-    if (this.#arrayIsEmpty(usersSearched)) return [];
-
-    const ids = this.#getIds(usersSearched);
-
-    const usersWithoutKnowledges = this.#getUserDataById(ids, usersSearched);
-
-    const usersWithKnowledges = this.#fillInUserKnowledge(
-      usersWithoutKnowledges,
-      usersSearched
-    );
-
-    return usersWithKnowledges;
-  }
-
-  #getUserDataById(ids, usersSearched) {
-    let user = {};
-    let userFound = {};
-
-    const users = ids.map((id) => {
-      userFound = usersSearched.find((userSearched) => userSearched.id === id);
-
-      user = { ...userFound };
-
-      delete user?.knowledge_name;
-      delete user?.knowledge_score;
-
-      return user;
-    });
-
-    return users;
-  }
-
-  #getIds(usersSearched) {
-    let ids = new Set();
-
-    usersSearched.forEach((userSearched) => {
-      ids.add(userSearched.id);
-    });
-
-    return Array.from(ids);
-  }
-
-  #fillInUserKnowledge(users, usersSearched) {
-    const usersWithKnowledges = users.map((user) => {
-      const usersFilteredById = usersSearched.filter(
-        (userSearched) => userSearched.id === user.id
-      );
-
-      const knowledges = usersFilteredById.map((userFiltered) => {
-        const { knowledge_name: name, knowledge_score: score } = userFiltered;
-
-        return {
-          name,
-          score,
-        };
-      });
-
-      user.knowledges = knowledges;
-      return user;
-    });
-    return usersWithKnowledges;
-  }
-
-  #arrayIsEmpty(array = []) {
-    return array?.length <= 0;
   }
 }
 
