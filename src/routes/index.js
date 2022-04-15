719356097662e99e9b76ace2beda3491c03b2f04
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-15 01:54:08 -0300

    tira tudo referente as variaveis de ambiente URL_API

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,6 @@ import { Router } from 'express';
 import UserController from '../controllers/User.controller';
 import swaggerUI from 'swagger-ui-express';
 import swaggerDocument from '../../swagger.json';
-import 'dotenv/config';
-
-const NODE_ENV = process.env.NODE_ENV === 'production';
-const URL_API = NODE_ENV
-  ? process.env.URL_API_PROD
-  : `${process.env.URL_API_DEV}:${process.env.PORT}`;
 
 const routes = new Router();
 const userController = new UserController();
@@ -17,7 +11,7 @@ routes.get('/', (req, res) => {
   res.send(`
     <body style="padding: 0;margin: 0;">
       <div style="display: flex;justify-content: center;align-items: center;width: 100vw;height: 100vh;">
-        <a href="${URL_API}/api-docs" style="font-famaily: 'Arial';font-size: 1.5rem;">Acessar a documentação da API Rest com Swagger</a>
+        <a href="/api-docs" style="font-famaily: 'Arial';font-size: 1.5rem;">Acessar a documentação da API Rest com Swagger</a>
       </div>
     </body>`);
 });
