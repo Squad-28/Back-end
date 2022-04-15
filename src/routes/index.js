652670746843423a00e36e65036ff0191f7b3c99
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-15 01:26:37 -0300

    src.routes: middleware para o swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/User.controller';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../../swagger.json';
 
 const routes = new Router();
 const userController = new UserController();
+
+routes.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 routes.get('/users', userController.index);
 routes.post('/users', userController.create);
