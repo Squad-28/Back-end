91fab3435f4b37ddee416e716c356891673a2bdb
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2022-04-27 00:08:48 -0300

    deleta pasta antiga de testes, yarn.lock, nodemon e .sequelizerc

--- test/deleteAllRecordsFromTables.ts
+++
@@ -1,9 +0,0 @@
-import Knowledge from '../src/models/Knowledge';
-import KnowledgeList from '../src/models/KnowledgeList';
-import User from '../src/models/User';
-
-export async function deleteAllRecordsFromTables(models = []) {
-  await KnowledgeList.sync({ force: true });
-  await User.sync({ force: true });
-  await Knowledge.sync({ force: true });
-}
